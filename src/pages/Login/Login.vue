df6dc8b27e22b987b8a1992d669e658bf81752e6
Vinícius Marcili <vinipetter@gmail.com>
2018-10-23 09:03:49 -0300

    :construction: Html Base Login

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,13 +1,52 @@
 <template>
     <div class="content" id="login">
-      
+        <b-card title="Login">
+          <b-form v-on:submit.prevent="onSubmit">
+            <b-form-input 
+              id="email"
+              type="email"
+              v-model="form.email"
+              required
+              placeholder="E-mail"
+            />
+            <b-form-input 
+              id="password"
+              type="password"
+              v-model="form.password"
+              required
+              placeholder="Senha"
+            />
+            <b-button type='submit'>
+              Save
+            </b-button>
+          </b-form>
+          <b-link href="#">
+            Esqueci minha senha
+          </b-link>
+        </b-card>
+        <span id='errors'>
+          erros aqui
+        </span>
     </div>
 </template>
 
 <script>
-  export default {
-    name: 'Login'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      alert('login')
+    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
