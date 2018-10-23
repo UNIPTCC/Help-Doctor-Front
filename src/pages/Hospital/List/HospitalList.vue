75cfb5b5e81aff10c084a8a098e5c34f2969b757
Vinícius Marcili <vinipetter@gmail.com>
2018-10-23 14:02:12 -0300

    :construction: Hospital List

--- src/pages/Hospital/List/HospitalList.vue
+++ src/pages/Hospital/List/HospitalList.vue
@@ -2,6 +2,39 @@
  <div>
     <header-default />
     <div class="content" id="hospital-list">
+      <b-container>
+        <b-row>
+          <b-col>
+            <b-form-input v-model="filter" placeholder="Filtrar" />
+            <b-btn :disabled="!filter" @click="filter = ''">X</b-btn>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-table
+              responsive
+              striped
+              hover
+              stacked="lg"
+              :items="hospitals"
+              :fields="colunms"
+              :current-page="currentPage"
+              :per-page="perPage"
+              :filter="filter"
+              @filtered="onFiltered"
+            />
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="6">
+            <b-pagination
+              :total-rows="totalRows"
+              :per-page="perPage"
+              v-model="currentPage"
+            />
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
     <footer-default />
   </div>
@@ -9,7 +42,51 @@
 
 <script>
 export default {
-  name: 'HospitalList'
+  name: 'HospitalList',
+  data () {
+    return {
+      colunms: [
+        {
+          key: 'id',
+          label: 'ID',
+          sortable: true
+        },
+        {
+          key: 'name',
+          label: 'Nome',
+          sortable: true
+        }
+      ],
+      hospitals: [
+        {
+          id: 1,
+          name: 'Hospital 1',
+        },
+        {
+          id: 2,
+          name: 'Hospital 2',
+        },
+        {
+          id: 3,
+          name: 'Order Hospital 3',
+        }
+      ],
+      stacked: (window.innerWidth > 991) ? false : true,
+      currentPage: 1,
+      perPage: 1,
+      totalRows: 1,
+      filter: null
+    }
+  },
+  created() {
+    this.totalRows = this.hospitals.length
+  },
+  methods: {
+    onFiltered (filteredItems) {
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
+    }
+  }
 }
 </script>
 
