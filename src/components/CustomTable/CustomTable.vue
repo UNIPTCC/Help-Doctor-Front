2ad30e0bae8a02d456a94f08a7ae41246d41c507
Vinícius Marcili <vinipetter@gmail.com>
2018-10-24 12:58:47 -0300

    :lipstick: Fix Scoped Classes

--- src/components/CustomTable/CustomTable.vue
+++ src/components/CustomTable/CustomTable.vue
@@ -79,7 +79,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "../../assets/styles/globals";
 @import "./CustomTable";
 </style>
--- src/pages/Hospital/List/HospitalList.vue
+++ src/pages/Hospital/List/HospitalList.vue
@@ -77,7 +77,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../../../assets/styles/globals";
 @import "./HospitalList";
 </style>
